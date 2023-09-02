5a3b1b657d61b6ed3e242ae5373d989603da4acb
anthonyDev01 <86266996+anthonyDev01@users.noreply.github.com>
2023-09-02 02:29:41 -0300

    finalizando o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Aluno(val nome : String, val idade : Int)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracaoMin: Int, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(alunos: List<Aluno>) {
+        for(aluno in alunos){
+            inscritos.add(aluno)
+        }   
+    }
+    
+    fun listarAlunos(){
+        for(aluno in inscritos){
+        	println("Nome: ${aluno.nome}; Idade: ${aluno.idade}")
+    	}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Criando novos alunos
+    
+    val aluno1 : Aluno = Aluno("Anthony", 19)
+    val aluno2 : Aluno = Aluno("Maria", 29)
+    val aluno3 : Aluno = Aluno("Pedro", 47)
+    
+    //criando Conteudos Educacionais
+    
+    val conteudo1 : ConteudoEducacional = ConteudoEducacional("conhecendo o Kotlin e Sua Documentação Oficial", 120, Nivel.BASICO)
+    val conteudo2 : ConteudoEducacional = ConteudoEducacional("Tratamento de Exceções em Kotlin", 120, Nivel.INTERMEDIARIO)
+    val conteudo3 : ConteudoEducacional = ConteudoEducacional("O Poder das Funções em Kotlin", 120, Nivel.BASICO) 
+    
+    //criando Formacoes
+    
+    val formacao1 : Formacao = Formacao("Santander Bootcamp 2023 -  Mobile Android com Kotlin", listOf(conteudo1, conteudo2, conteudo3))
+    
+    //matriculando alunos
+    
+    formacao1.matricular(listOf(aluno1, aluno2, aluno3))
+    
+    
+    //Listando alunos
+    
+    formacao1.listarAlunos()
+    
+    
 }
